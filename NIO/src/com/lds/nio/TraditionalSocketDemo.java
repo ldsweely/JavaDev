58a90fe30ab4538609de268aefa7486d554d8239
ldsweely <ldsweely@users.noreply.github.com>
2018-02-06 14:16:48 +0800

    Initial commit

---
+++ NIO/src/com/lds/nio/TraditionalSocketDemo.java
@@ -0,0 +1,27 @@
+package com.lds.nio;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TraditionalSocketDemo {
+	public static void main(String[] args) throws IOException{
+		ServerSocket  serverSocket=  new ServerSocket(7777);
+		System.out.println("服务端启动。。。。");
+		while(true){
+			Socket socket = serverSocket.accept();
+			System.out.println("有新客户端连接上来了。。。。");
+			
+			InputStream is =  socket.getInputStream();
+			byte[] b= new byte[1024];
+			while(true){
+				int data = is.read();
+				if(data != -1)
+				{
+					String info = new String(b,0,data);
+					System.out.println(info);
+				}
+			}
+		}
+	}
+}
