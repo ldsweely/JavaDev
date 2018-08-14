91e4eb8aa04b766792218fdff7be4f891b172a31
liuds <ldsweely@hotmail.com>
2018-08-14 16:32:09 +0800

    x

--- bigdata_learning/shizhan_03_hadoop/src/cn/itcast/bigdata/hdfs/HdfsClientDemo.java
+++ bigdata_learning/shizhan_03_hadoop/src/cn/itcast/bigdata/hdfs/HdfsClientDemo.java
@@ -1,10 +1,13 @@
 package cn.itcast.bigdata.hdfs;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URI;
 import java.util.Iterator;
 import java.util.Map.Entry;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.BlockLocation;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.LocatedFileStatus;
@@ -28,83 +31,135 @@ public class HdfsClientDemo {
 	public void init() throws Exception{
 		
 		conf = new Configuration();
-		conf.set("fs.defaultFS", "hdfs://mini01:9000");
+//		conf.set("fs.defaultFS", "hdfs://mini1:9000");
+		conf.set("dfs.replication", "5");
 		
 		//拿到一个文件系统操作的客户端实例对象
-		/*fs = FileSystem.get(conf);*/
+		fs = FileSystem.get(conf);
 		//可以直接传入 uri和用户身份
-		fs = FileSystem.get(new URI("hdfs://mini01:9000"),conf,"hadoop"); //最后一个参数为用户名
+		fs = FileSystem.get(new URI("hdfs://mini01:9000"),conf,"hadoop");
 	}
 
+	/**
+	 * 上传文件
+	 * @throws Exception
+	 */
 	@Test
 	public void testUpload() throws Exception {
 		
-		Thread.sleep(2000);
-		fs.copyFromLocalFile(new Path("d:/test.log"), new Path("/access.log.copy"));
+		fs.copyFromLocalFile(new Path("c:/access.log"), new Path("/access.log.copy"));
 		fs.close();
 	}
 	
 	
+	/**
+	 * 下载文件
+	 * @throws Exception
+	 */
 	@Test
 	public void testDownload() throws Exception {
 		
-		fs.copyToLocalFile(new Path("/access.log.copy"), new Path("e:/"));
+		fs.copyToLocalFile(new Path("/access.log.copy"), new Path("d:/"));
+	}
+	
+	
+	/**
+	 * 打印参数
+	 */
+	@Test
+	public void testConf(){
+		
+		Iterator<Entry<String, String>> it = conf.iterator();
+		while(it.hasNext()){
+			Entry<String, String> ent = it.next();
+			System.out.println(ent.getKey() + " : " + ent.getValue());
+			
+		}
+		
+	}
+	
+	
+	@Test
+	public void testMkdir() throws Exception {
+		boolean mkdirs = fs.mkdirs(new Path("/testmkdir/aaa/bbb"));
+		System.out.println(mkdirs);
+		
+	}
+	
+	
+	@Test
+	public void testDelete() throws Exception {
+		
+		boolean flag = fs.delete(new Path("/testmkdir"), true);
+		System.out.println(flag);
+		
+	}
+	
+	
+	/**
+	 * 递归列出指定目录下所有子文件夹中的文件
+	 * @throws Exception
+	 */
+	@Test
+	public void testLs() throws Exception {
+		
+		RemoteIterator<LocatedFileStatus> listFiles = fs.listFiles(new Path("/"), true);
+		
+		while(listFiles.hasNext()){
+			LocatedFileStatus fileStatus = listFiles.next();
+			System.out.println("blocksize: " +fileStatus.getBlockSize());
+			System.out.println("owner: " +fileStatus.getOwner());
+			System.out.println("Replication: " +fileStatus.getReplication());
+			System.out.println("Permission: " +fileStatus.getPermission());
+			System.out.println("Name: " +fileStatus.getPath().getName());
+			System.out.println("------------------");
+			BlockLocation[] blockLocations = fileStatus.getBlockLocations();
+			for(BlockLocation b:blockLocations){
+				System.out.println("块起始偏移量: " +b.getOffset());
+				System.out.println("块长度:" + b.getLength());
+				//块所在的datanode节点
+				String[] datanodes = b.getHosts();
+				for(String dn:datanodes){
+				System.out.println("datanode:" + dn);
+				}
+			}
+			
+			
+			
+		}
+		
+	}
+	
+	
+	
+	@Test
+	public void testLs2() throws Exception {
+		
+		FileStatus[] listStatus = fs.listStatus(new Path("/"));
+		for(FileStatus file :listStatus){
+			
+			System.out.println("name: " + file.getPath().getName());
+			System.out.println((file.isFile()?"file":"directory"));
+			
+		}
+		
+	}
+	
+	
+	
+	
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		conf.set("fs.defaultFS", "hdfs://mini1:9000");
+		//拿到一个文件系统操作的客户端实例对象
+		FileSystem fs = FileSystem.get(conf);
+		
+		fs.copyFromLocalFile(new Path("c:/access.log"), new Path("/access.log.copy"));
 		fs.close();
 	}
 	
-	@Test
-	public void testConf(){
-		Iterator<Entry<String, String>> iterator = conf.iterator();
-		while (iterator.hasNext()) {
-			Entry<String, String> entry = iterator.next();
-			System.out.println(entry.getValue() + "--" + entry.getValue());//conf加载的内容
-		}
-	}
-	
-	/**
-	 * 创建目录
-	 */
-	@Test
-	public void makdirTest() throws Exception {
-		boolean mkdirs = fs.mkdirs(new Path("/aaa/bbb"));
-		System.out.println(mkdirs);
-	}
-	
-	/**
-	 * 删除
-	 */
-	@Test
-	public void deleteTest() throws Exception{
-		boolean delete = fs.delete(new Path("/aaa"), true);//true， 递归删除
-		System.out.println(delete);
-	}
-	
-	@Test
-	public void listTest() throws Exception{
-		
-		FileStatus[] listStatus = fs.listStatus(new Path("/"));
-		for (FileStatus fileStatus : listStatus) {
-			System.err.println(fileStatus.getPath()+"================="+fileStatus.toString());
-		}
-		//会递归找到所有的文件
-		RemoteIterator<LocatedFileStatus> listFiles = fs.listFiles(new Path("/"), true);
-		while(listFiles.hasNext()){
-			LocatedFileStatus next = listFiles.next();
-			String name = next.getPath().getName();
-			Path path = next.getPath();
-			System.out.println(name + "---" + path.toString());
-		}
-	}
-	
-//	public static void main(String[] args) throws Exception {
-//		Configuration conf = new Configuration();
-//		conf.set("fs.defaultFS", "hdfs://mini01:9000");
-//		//拿到一个文件系统操作的客户端实例对象
-//		FileSystem fs = FileSystem.get(conf);
-//		
-//		fs.copyFromLocalFile(new Path("d:/test.log"), new Path("/access.log.copy"));
-//		fs.close();
-//	}
-	
 
+	
+	
+	
 }
